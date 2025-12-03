6ee8ed1f70ccd1fc9bbd902bb2440828677d08e8
Ritumani Das <ritumaniidas@gmail.com>
2025-12-03 02:44:40 +0000

    setup dbt project

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,6 +1,5 @@
-select TIME,dayname(TIME) Day_name,
+select 
+TIME,dayname(TIME) as Day_name,
 case when dayname(TIME) in('Sat','Sun') then 'Weekend'
     else 'Weekday' end as Day_state
-from {{ source('demo', 'weather') }};
-
-
+from {{ source('demo', 'weather') }}
